80b0711580033af522ee4c4474923bf29bc96c01
Sheri Kwong <sherikwo@buffalo.edu>
2018-02-11 20:57:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+  if (string.toLowerCase() = string) {
+    return "I can't hear you!"
+  }
 }
 
 describe('sayHiToGrandma(string)', function() {
